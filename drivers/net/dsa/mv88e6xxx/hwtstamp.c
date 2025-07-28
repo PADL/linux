e11dc4d20497aaee971c2b6394dc700064ac324e
Luke Howard <lukeh@padl.com>
2025-07-28 14:22:49 +1000

    net: dsa: mv88e6352: support for filtering 802.1AS PTP frames
    linux/net_tstamp.h implies HWTSTAMP_FILTER_PTP_V2_L2_EVENT is for 802.1AS PTP
    packets only, not all L2 IEEE 1588 packets. If this filter is set, set the
    802.1AS transportSpecific filter on the switch.
    
    Not for upstream.

--- drivers/net/dsa/mv88e6xxx/hwtstamp.c
+++ drivers/net/dsa/mv88e6xxx/hwtstamp.c
@@ -128,12 +128,14 @@ static int mv88e6xxx_set_hwtstamp_config(struct mv88e6xxx_chip *chip, int port,
 	case HWTSTAMP_FILTER_NONE:
 		tstamp_enable = false;
 		break;
-	case HWTSTAMP_FILTER_PTP_V2_L4_EVENT:
-	case HWTSTAMP_FILTER_PTP_V2_L4_SYNC:
-	case HWTSTAMP_FILTER_PTP_V2_L4_DELAY_REQ:
 	case HWTSTAMP_FILTER_PTP_V2_L2_EVENT:
 	case HWTSTAMP_FILTER_PTP_V2_L2_SYNC:
 	case HWTSTAMP_FILTER_PTP_V2_L2_DELAY_REQ:
+		config->rx_filter = HWTSTAMP_FILTER_PTP_V2_L2_EVENT;
+		break;
+	case HWTSTAMP_FILTER_PTP_V2_L4_EVENT:
+	case HWTSTAMP_FILTER_PTP_V2_L4_SYNC:
+	case HWTSTAMP_FILTER_PTP_V2_L4_DELAY_REQ:
 	case HWTSTAMP_FILTER_PTP_V2_EVENT:
 	case HWTSTAMP_FILTER_PTP_V2_SYNC:
 	case HWTSTAMP_FILTER_PTP_V2_DELAY_REQ:
@@ -151,10 +153,10 @@ static int mv88e6xxx_set_hwtstamp_config(struct mv88e6xxx_chip *chip, int port,
 		if (chip->enable_count == 1 && ptp_ops->global_enable)
 			err = ptp_ops->global_enable(chip);
 		if (!err && ptp_ops->port_enable)
-			err = ptp_ops->port_enable(chip, port);
+			err = ptp_ops->port_enable(chip, port, config);
 	} else {
 		if (ptp_ops->port_disable)
-			err = ptp_ops->port_disable(chip, port);
+			err = ptp_ops->port_disable(chip, port, config);
 		chip->enable_count -= 1;
 		if (chip->enable_count == 0 && ptp_ops->global_disable)
 			ptp_ops->global_disable(chip);
@@ -404,7 +406,7 @@ bool mv88e6xxx_port_rxtstamp(struct dsa_switch *ds, int port,
 	chip = ds->priv;
 	ps = &chip->port_hwtstamp[port];
 
-	if (ps->tstamp_config.rx_filter != HWTSTAMP_FILTER_PTP_V2_EVENT)
+	if (ps->tstamp_config.rx_filter == HWTSTAMP_FILTER_NONE)
 		return false;
 
 	hdr = mv88e6xxx_should_tstamp(chip, port, skb, type);
@@ -606,7 +608,8 @@ int mv88e6165_global_enable(struct mv88e6xxx_chip *chip)
 	return mv88e6xxx_ptp_write(chip, MV88E6165_PTP_CFG, val);
 }
 
-int mv88e6352_hwtstamp_port_disable(struct mv88e6xxx_chip *chip, int port)
+int mv88e6352_hwtstamp_port_disable(struct mv88e6xxx_chip *chip, int port,
+				    struct kernel_hwtstamp_config *config)
 {
 	int err;
 
@@ -630,8 +633,10 @@ static u16 mv88e6xxx_arr_ts_cfg2(struct mv88e6xxx_chip *chip)
 		MV88E6XXX_PORT_PTP_CFG2_ARR_TS_DISABLED;
 }
 
-int mv88e6352_hwtstamp_port_enable(struct mv88e6xxx_chip *chip, int port)
+int mv88e6352_hwtstamp_port_enable(struct mv88e6xxx_chip *chip, int port,
+				   struct kernel_hwtstamp_config *config)
 {
+	u16 val;
 	int err;
 
 	err = mv88e6xxx_port_ptp_write(chip, port, MV88E6XXX_PORT_PTP_CFG2,
@@ -639,8 +644,12 @@ int mv88e6352_hwtstamp_port_enable(struct mv88e6xxx_chip *chip, int port)
 	if (err)
 		return err;
 
-	err = mv88e6xxx_port_ptp_write(chip, port, MV88E6XXX_PORT_PTP_CFG0,
-				       MV88E6XXX_PORT_PTP_CFG0_DISABLE_TSPEC_MATCH);
+	if (config->rx_filter == HWTSTAMP_FILTER_PTP_V2_L2_EVENT)
+		val = MV88E6XXX_PORT_PTP_CFG0_TSPEC_8021AS;
+	else
+		val = MV88E6XXX_PORT_PTP_CFG0_DISABLE_TSPEC_MATCH;
+
+	err = mv88e6xxx_port_ptp_write(chip, port, MV88E6XXX_PORT_PTP_CFG0, val);
 	if (err)
 		return err;
 
@@ -658,7 +667,7 @@ static int mv88e6xxx_hwtstamp_port_setup(struct mv88e6xxx_chip *chip, int port)
 	skb_queue_head_init(&ps->rx_queue2);
 
 	if (ptp_ops->port_disable)
-		return ptp_ops->port_disable(chip, port);
+		return ptp_ops->port_disable(chip, port, &ps->tstamp_config);
 
 	return 0;
 }
